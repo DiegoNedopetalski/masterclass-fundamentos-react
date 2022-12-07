11d456469da617b3a1d01692d92def6c9cc84dd7
DiegoNedopetalski <diegoluis.nedo@gmail.com>
2022-12-07 20:13:52 -0300

    masterclass-react-cod3r

---
+++ src/components/Primeiro.jsx
@@ -0,0 +1,8 @@
+export default function Primeiro() {
+    return (
+        <div>
+            <h1>Primeiro Componente</h1>
+            <h2>Exemplo de um componente React</h2>
+        </div>
+    )
+}
