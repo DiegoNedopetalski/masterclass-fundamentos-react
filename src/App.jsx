693262d5fc2fd0aeb79ea1429815b18e2e3a9504
DiegoNedopetalski <diegoluis.nedo@gmail.com>
2022-12-08 17:19:19 -0300

    mais exemplos

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import ComParametro from './components/ComParametro'
 import Card from './components/layout/Card'
 import Primeiro from './components/Primeiro'
 import './App.css'
+import Repeticao from './components/Repeticao'
+import Condicional from './components/Condicional'
 
 export default function App(props) {
     return (
@@ -27,6 +29,15 @@ export default function App(props) {
                         <li>Maria</li>
                     </ul>
                 </ComFilhos>
+            </Card>
+            <Card titulo="#04 - Repetição" >
+                <Repeticao />
+            </Card>
+            <Card titulo="#05 - Condicional" >
+                <Condicional numero={11}/>
+            </Card>
+            <Card titulo="#06 - Condicional com IF" >
+                <Condicional numero={10}/>
             </Card>
         </div>
     )
---
+++ src/data/products.js
@@ -0,0 +1,7 @@
+const products = [
+    { id: 1, name: 'Caneta', price: 7.49 },
+    { id: 1, name: 'Lapis', price: 2.99 },
+    { id: 1, name: 'Caderno', price: 19.49 }
+]
+
+export default products
