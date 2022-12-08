e77c4736de5355f52cc3e5a19c461c62cd0a7703
DiegoNedopetalski <diegoluis.nedo@gmail.com>
2022-12-08 17:19:06 -0300

    mais exemplos

---
+++ src/components/Repeticao.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import products from '../data/products'
+
+export default function Repeticao(props) {
+
+    function getProductLI() {
+        return products.map(prod  => {
+            return <li key={prod.id}>{prod.id} {prod.name} - R$ {prod.price}</li>
+        })
+    }
+
+    return (
+        <div>
+            <h2>Repetição</h2>
+            <ul>
+                {getProductLI()}
+            </ul>
+        </div>
+    )
+}
